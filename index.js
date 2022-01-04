b049b1e01288aff5db1298e7effcce29b2d9ec94
Jhonny Rodriguez Martinez <71800608+Jyro77@users.noreply.github.com>
2022-01-04 19:22:24 -0400

    Complete the Register Process
    adding users to the db
    complete the register view page
    encrypting pass process
    and more

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const User = require('./models/user');
+const bcrypt = require('bcrypt')
+
+mongoose.connect('mongodb://localhost:27017/authDemo').
+    then(()=>{
+        console.log('DATABASE CONNECTED');
+    })
+    .catch(err =>{
+        console.log('Connection Error');
+        console.log(err);
+    })
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('This is the home page!');
+})
+
+app.get('/register', (req, res) => {
+    res.render('register')
+})
+
+app.post('/register', async (req, res) => {
+    const { password, username } = req.body;
+    const hash = await bcrypt.hash(password, 12);
+    const user = new User({
+        username,
+        password: hash
+    })
+    await user.save();
+    res.redirect('/');
+})
 
 app.get('/secret', (req, res) => {
     res.send('This is secret! You cannot see me unless you are logged in!')
